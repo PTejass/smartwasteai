dd321a08aef05558cd8abf532a0aa594aca334c3
P B TEJAS <pbtejas10bvhs@gmail.com>
2025-05-24 21:55:10 +0530

    removedconsolelog

--- src/components/WasteGuide.tsx
+++ src/components/WasteGuide.tsx
@@ -7,7 +7,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log(supabaseUrl, supabaseAnonKey);
 interface WasteGuideProps {
   location: string;
   searchQuery: string;
